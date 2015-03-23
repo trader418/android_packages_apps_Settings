5c81b8774442c0133302679947d316a8225fdf4f
Janson Kang <temasek71@gmail.com>
2015-03-23 12:07:19 +0800

    Integrate OTA update into Settings
    Signed-off-by: Janson Kang <temasek71@gmail.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1316,6 +1316,17 @@ public class SettingsActivity extends Activity
                             UserManager.DISALLOW_DEBUGGING_FEATURES)) {
                         removeTile = true;
                     }
+                } else if (id == R.id.ota_settings) {
+                    // Embedding into Settings only if app exists (user could manually remove it)
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("com.ota.updates", 0).versionCode >= 0);
+                    } catch (PackageManager.NameNotFoundException e) {
+ 
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                    }
                 } else if (id == R.id.supersu_settings) {
                     // Embedding into Settings is supported from SuperSU v1.85 and up
                     boolean supported = false;

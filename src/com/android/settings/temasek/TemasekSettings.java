bf8f87748c7ee1d088a04ff0bba95e5e2084af63
Janson Kang <temasek71@gmail.com>
2015-04-05 20:27:40 +0800

    Initial bring up of Temasek Spare Parts
    Signed-off-by: Janson Kang <temasek71@gmail.com>

---
+++ src/com/android/settings/temasek/TemasekSettings.java
@@ -0,0 +1,38 @@
+package com.android.settings.temasek;
+
+import android.os.Bundle;
+import android.content.res.Resources;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+import android.provider.Settings;
+
+import com.android.internal.logging.MetricsLogger;
+
+public class TemasekSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "TemasekSettings";
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.APPLICATION;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.temasek_settings);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        return false;
+    }
+}

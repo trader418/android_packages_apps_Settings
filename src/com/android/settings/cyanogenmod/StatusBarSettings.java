275b20831836d665166ef82bf6387dce6c27c68d
Bajee11 <bajee11@gmail.com>
2015-06-12 18:04:00 +0300

    Settings: Breathing missedcall/voicemail
    Updated for Marshmallow by DU
    
    Change-Id: Iea42c47ce19428fa8759959a26ba02c64b35c32d

--- src/com/android/settings/cyanogenmod/StatusBarSettings.java
+++ src/com/android/settings/cyanogenmod/StatusBarSettings.java
@@ -26,6 +26,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.preference.ListPreference;
@@ -83,6 +84,8 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     private static final String PREF_QS_TRANSPARENT_SHADE = "qs_transparent_shade";
     private static final String PREF_QS_TRANSPARENT_HEADER = "qs_transparent_header";
     private static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
+    private static final String MISSED_CALL_BREATH = "missed_call_breath";
+    private static final String VOICEMAIL_BREATH = "voicemail_breath";
 
     private static final int STATUS_BAR_BATTERY_STYLE_HIDDEN = 4;
     private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 6;
@@ -104,13 +107,13 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     private ListPreference mFontStyle;
     private ListPreference mStatusBarClockFontSize;
     private ListPreference mCustomHeaderDefault;
-
     private ListPreference mStatusBarBattery;
     private ListPreference mStatusBarBatteryShowPercent;
     private ListPreference mQuickPulldown;
-
     private SwitchPreference mEnableTaskManager;
     private SwitchPreference mBlockOnSecureKeyguard;
+    private SwitchPreference mMissedCallBreath;
+    private SwitchPreference mVoicemailBreath;
 
     private SeekBarPreference mQSShadeAlpha;
     private SeekBarPreference mQSHeaderAlpha;
@@ -133,6 +136,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
         final CmLockPatternUtils lockPatternUtils = new CmLockPatternUtils(getActivity());
+        Context context = getActivity();
 
         Resources res = getResources();
         PackageManager pm = getPackageManager();
@@ -286,6 +290,27 @@ public class StatusBarSettings extends SettingsPreferenceFragment
                     res.getString(R.string.status_bar_quick_qs_pulldown_summary, direction));
         }
         mQuickPulldown.setOnPreferenceChangeListener(this);
+
+        // Breathing Notifications
+        mMissedCallBreath = (SwitchPreference) findPreference(MISSED_CALL_BREATH);
+        mVoicemailBreath = (SwitchPreference) findPreference(VOICEMAIL_BREATH);
+
+        ConnectivityManager cm = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE)) {
+
+            mMissedCallBreath.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.KEY_MISSED_CALL_BREATH, 0) == 1);
+            mMissedCallBreath.setOnPreferenceChangeListener(this);
+
+            mVoicemailBreath.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.KEY_VOICEMAIL_BREATH, 0) == 1);
+            mVoicemailBreath.setOnPreferenceChangeListener(this);
+        } else {
+            prefSet.removePreference(mMissedCallBreath);
+            prefSet.removePreference(mVoicemailBreath);
+        }
 
         setHasOptionsMenu(true);
         mCheckPreferences = true;
@@ -466,6 +491,14 @@ public class StatusBarSettings extends SettingsPreferenceFragment
                 mQuickPulldown.setSummary(
                         res.getString(R.string.status_bar_quick_qs_pulldown_summary, direction));
             }
+            return true;
+        } else if (preference == mMissedCallBreath) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver, Settings.System.KEY_MISSED_CALL_BREATH, value ? 1 : 0);
+            return true;
+        } else if (preference == mVoicemailBreath) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver, Settings.System.KEY_VOICEMAIL_BREATH, value ? 1 : 0);
             return true;
         }
         return false;

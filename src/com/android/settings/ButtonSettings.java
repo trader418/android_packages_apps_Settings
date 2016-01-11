929595708d3463abd5b90da5da743b891df049b6
Adnan Begovic <adnan@cyngn.com>
2016-01-11 13:58:26 -0800

    Settings: Add CMMetricsLogger constants to proper pages.
    Change-Id: I6e1d0ec2be4c6da51267c163575472827c9c50cd
    
    Conflicts:
    	src/com/android/settings/cmstats/AnonymousStats.java
    	src/com/android/settings/cmstats/PreviewData.java

--- src/com/android/settings/ButtonSettings.java
+++ src/com/android/settings/ButtonSettings.java
@@ -44,8 +44,6 @@ import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.WindowManagerGlobal;
 
-import com.android.internal.logging.MetricsLogger;
-
 import com.android.settings.cyanogenmod.ButtonBacklightBrightness;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -56,6 +54,7 @@ import com.android.settings.Utils;
 import cyanogenmod.hardware.CMHardwareManager;
 import cyanogenmod.providers.CMSettings;
 
+import org.cyanogenmod.internal.logging.CMMetricsLogger;
 import org.cyanogenmod.internal.util.ScreenType;
 
 import java.util.List;
@@ -608,7 +607,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 
     @Override
     protected int getMetricsCategory() {
-        return MetricsLogger.DONT_TRACK_ME_BRO;
+        return CMMetricsLogger.BUTTON_SETTINGS;
     }
 
    private void updateNavBarSettings() {

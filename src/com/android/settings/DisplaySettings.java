AICP/packages_apps_Settings

Dashboard Text Size 1/2
Options for title and category text size

Change-Id: If043f5637aeb0d8eafc2460d9512f15815f7f89c

src/com/android/settings/DisplaySettings.java
@@ -71,6 +71,7 @@ import java.util.List;
 import com.android.settings.Utils;
 import com.android.settings.cyanogenmod.DisplayRotation;
 import com.android.settings.dashboard.DashboardContainerView;
+import com.android.settings.widget.SeekBarPreferenceCham;
 
 import cyanogenmod.hardware.LiveDisplayManager;
 import cyanogenmod.providers.CMSettings;
@@ -106,6 +107,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String DASHBOARD_COLUMNS = "dashboard_columns";
     private static final String DASHBOARD_SWITCHES = "dashboard_switches";
     private static final String DASHBOARD_FONT_STYLE = "dashboard_font_style";
+    private static final String SETTINGS_TITLE_TEXT_SIZE  = "settings_title_text_size";
+    private static final String SETTINGS_CATEGORY_TEXT_SIZE  = "settings_category_text_size";
     private static final String KEY_LIVEDISPLAY = "live_display";
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
@@ -128,6 +131,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private ListPreference mDashboardColumns;
     private ListPreference mDashboardSwitches;
     private ListPreference mDashFontStyle;
+    private SeekBarPreferenceCham mDashTitleTextSize;
+    private SeekBarPreferenceCham mDashCategoryTextSize;
 
     private ContentObserver mAccelerometerRotationObserver =
             new ContentObserver(new Handler()) {
@@ -208,6 +213,18 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                 getContentResolver(), Settings.System.DASHBOARD_SWITCHES, 0)));
         mDashboardSwitches.setSummary(mDashboardSwitches.getEntry());
         mDashboardSwitches.setOnPreferenceChangeListener(this);
+
+        mDashTitleTextSize =
+                (SeekBarPreferenceCham) findPreference(SETTINGS_TITLE_TEXT_SIZE);
+        mDashTitleTextSize.setValue(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.SETTINGS_TITLE_TEXT_SIZE, 14));
+        mDashTitleTextSize.setOnPreferenceChangeListener(this);
+
+        mDashCategoryTextSize =
+                (SeekBarPreferenceCham) findPreference(SETTINGS_CATEGORY_TEXT_SIZE);
+        mDashCategoryTextSize.setValue(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.SETTINGS_CATEGORY_TEXT_SIZE, 15));
+        mDashCategoryTextSize.setOnPreferenceChangeListener(this);
 
         mDashFontStyle = (ListPreference) findPreference(DASHBOARD_FONT_STYLE);
         mDashFontStyle.setOnPreferenceChangeListener(this);
@@ -657,6 +674,18 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.DASHBOARD_FONT_STYLE, val);
             mDashFontStyle.setSummary(mDashFontStyle.getEntries()[index]);
+            return true;
+        } 
+        if (preference == mDashTitleTextSize) {
+            int width = ((Integer)objValue).intValue();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SETTINGS_TITLE_TEXT_SIZE, width);
+            return true;
+        } 
+        if (preference == mDashCategoryTextSize) {
+            int width = ((Integer)objValue).intValue();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SETTINGS_CATEGORY_TEXT_SIZE, width);
             return true;
         }
         return true;

trader418/android_packages_apps_Settings

src/com/android/settings/DisplaySettings.java
@@ -85,6 +85,7 @@ import java.util.List;
 import com.android.settings.Utils;
 import com.android.settings.cyanogenmod.DisplayRotation;
 import com.android.settings.dashboard.DashboardContainerView;
+import com.android.settings.temasek.SeekBarPreference;
 
 import cyanogenmod.hardware.LiveDisplayManager;
 import cyanogenmod.providers.CMSettings;
@@ -122,6 +123,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String DASHBOARD_COLUMNS = "dashboard_columns";
     private static final String DASHBOARD_SWITCHES = "dashboard_switches";
     private static final String DASHBOARD_FONT_STYLE = "dashboard_font_style";
+    private static final String SETTINGS_TITLE_TEXT_SIZE  = "settings_title_text_size";
+    private static final String SETTINGS_CATEGORY_TEXT_SIZE  = "settings_category_text_size";
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
@@ -144,6 +147,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private ListPreference mDashboardColumns;
     private ListPreference mDashboardSwitches;
     private ListPreference mDashFontStyle;
+    private SeekBarPreference mDashTitleTextSize;
+    private SeekBarPreference mDashCategoryTextSize;
 
     private ContentObserver mAccelerometerRotationObserver =
             new ContentObserver(new Handler()) {
@@ -263,6 +268,18 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                 getContentResolver(), Settings.System.DASHBOARD_SWITCHES, 0)));
         mDashboardSwitches.setSummary(mDashboardSwitches.getEntry());
         mDashboardSwitches.setOnPreferenceChangeListener(this);
+
+        mDashTitleTextSize =
+                (SeekBarPreference) findPreference(SETTINGS_TITLE_TEXT_SIZE);
+        mDashTitleTextSize.setValue(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.SETTINGS_TITLE_TEXT_SIZE, 14));
+        mDashTitleTextSize.setOnPreferenceChangeListener(this);
+
+        mDashCategoryTextSize =
+                (SeekBarPreference) findPreference(SETTINGS_CATEGORY_TEXT_SIZE);
+        mDashCategoryTextSize.setValue(Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.SETTINGS_CATEGORY_TEXT_SIZE, 15));
+        mDashCategoryTextSize.setOnPreferenceChangeListener(this);
 
         mDashFontStyle = (ListPreference) findPreference(DASHBOARD_FONT_STYLE);
         mDashFontStyle.setOnPreferenceChangeListener(this);
@@ -789,6 +806,18 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.DASHBOARD_FONT_STYLE, val);
             mDashFontStyle.setSummary(mDashFontStyle.getEntries()[index]);
+            return true;
+        } 
+        if (preference == mDashTitleTextSize) {
+            int width = ((Integer)objValue).intValue();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SETTINGS_TITLE_TEXT_SIZE, width);
+            return true;
+        } 
+        if (preference == mDashCategoryTextSize) {
+            int width = ((Integer)objValue).intValue();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SETTINGS_CATEGORY_TEXT_SIZE, width);
             return true;
         }
         return true;